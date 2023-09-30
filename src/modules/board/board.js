e7a613eeb64ab9dc67b50a875dba9ad060d507ee
endulum <71853916+endulum@users.noreply.github.com>
2023-09-30 12:23:28 -0700

    focused on cell behavior

--- src/modules/board/board.js
+++ src/modules/board/board.js
@@ -1,21 +1,31 @@
+const Ship = require('../ship/ship.js')
+
 module.exports = class Board {
   #cells;
   get cells() { return this.#cells }
 
   constructor() {
-    this.#cells = Array(10).fill(Array(10).fill(null));
+    this.#cells = new Array(10).fill(0).map(() => new Array(10).fill(null));
+  }
+
+  changeCell(x, y, value) {
+    this.#cells[x].splice(y, 1, value);
   }
 
   print() {
     let toPrint = ``;
-    
+
     this.#cells.forEach(row => {
-      this.#cells.forEach(cell => {
-        toPrint += `■`;
+      row.forEach(cell => {
+        switch (cell) {
+          case 'hit': toPrint += `■ `; break;
+          default: toPrint += `□ `;
+        }
       });
+
       toPrint += `\n`;
     });
 
-    return toPrint;
+    console.log(toPrint);
   }
 }
--- src/modules/board/board.test.js
+++ src/modules/board/board.test.js
@@ -5,10 +5,17 @@ describe('board functions', () => {
 
   beforeAll(() => { board = new Board() });
 
-  describe('board initialization', () => {
+  describe('cell behavior', () => {
     test('has ten rows and 100 cells total', () => {
+      //board.print();
       expect(board.cells.length).toBe(10);
       expect(board.cells.reduce((acc, curr) => acc + curr.length, 0)).toBe(100);
-    })
-  })
-})
+    });
+
+    test('can change a cell', () => {
+      board.changeCell(3, 3, 'hit');
+      //board.print();
+      expect(board.cells.some(row => row.includes('hit'))).toBe(true);
+    });
+  });
+});

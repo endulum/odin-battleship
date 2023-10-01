bca505dc4e1a8cd3d1e57063ad610b720df5829f
endulum <71853916+endulum@users.noreply.github.com>
2023-09-30 19:46:55 -0700

    focused on ship placement

--- src/modules/board/board.js
+++ src/modules/board/board.js
@@ -1,11 +1,54 @@
 const Ship = require('../ship/ship.js')
 
 module.exports = class Board {
+  #ships = [];
+  get ships() { return this.#ships }
+
   #cells;
   get cells() { return this.#cells }
 
+  #occupiedCells = {}
+  get occupiedCells() { return this.#occupiedCells }
+
+  #hitCells = [];
+  get hitCells() { return this.#hitCells }
+
   constructor() {
-    this.#cells = new Array(10).fill(0).map(() => new Array(10).fill(null));
+    this.#cells = new Array(10).fill(0).map(() => new Array(10).fill('empty'));
+
+    [
+      ['Carrier', 5],
+      ['Battleship', 4],
+      ['Destroyer', 3],
+      ['Submarine', 3],
+      ['Patrol Boat', 2]
+    ].forEach(ship => {
+      this.#ships.push(new Ship(ship[0], ship[1]));
+      this.#occupiedCells[ship[0]] = [];
+    });
+  }
+
+  random() {
+    return Math.ceil(Math.random() * 9);
+  }
+
+  placeShip(x, y, ship, orientation = 'horizontal') {
+    const placedCells = [];
+    if (orientation === 'horizontal') {
+      if (x + ship.length < 10) {
+        for (let i = 0; i < ship.length; i++) {
+          placedCells.push([x + i, y]);
+        }
+      }
+    }
+    if (orientation === 'vertical') {
+      if (y + ship.length < 10) {
+        for (let i = 0; i < ship.length; i++) {
+          placedCells.push([x, y + i]);
+        }
+      }
+    }
+    return placedCells;
   }
 
   changeCell(x, y, value) {
@@ -14,17 +57,6 @@ module.exports = class Board {
 
   print() {
     let toPrint = ``;
-
-    this.#cells.forEach(row => {
-      row.forEach(cell => {
-        switch (cell) {
-          case 'hit': toPrint += `■ `; break;
-          default: toPrint += `□ `;
-        }
-      });
-
-      toPrint += `\n`;
-    });
 
     console.log(toPrint);
   }
--- src/modules/board/board.test.js
+++ src/modules/board/board.test.js
@@ -1,4 +1,5 @@
 const Board = require('./board');
+const Ship = require('../ship/ship.js')
 
 describe('board functions', () => {
   let board;
@@ -7,15 +8,39 @@ describe('board functions', () => {
 
   describe('cell behavior', () => {
     test('has ten rows and 100 cells total', () => {
-      //board.print();
       expect(board.cells.length).toBe(10);
       expect(board.cells.reduce((acc, curr) => acc + curr.length, 0)).toBe(100);
     });
 
     test('can change a cell', () => {
       board.changeCell(3, 3, 'hit');
-      //board.print();
       expect(board.cells.some(row => row.includes('hit'))).toBe(true);
     });
   });
+
+  describe('ship behavior', () => {
+    test('initialized with all new ships', () => {
+      // console.log(board.occupiedCells);
+      // board.print();
+      expect(board.ships.map(ship => [ship.name, ship.length, ship.hits])).toEqual([
+        ['Carrier', 5, 0],
+        ['Battleship', 4, 0],
+        ['Destroyer', 3, 0],
+        ['Submarine', 3, 0],
+        ['Patrol Boat', 2, 0]
+      ]); 
+    });
+
+    test('can place a ship horizontally', () => {
+      expect(board.placeShip(3, 3, new Ship('Destroyer', 3))).toEqual([[3, 3], [4, 3], [5, 3]]);
+    });
+
+    test('can place a ship vertically', () => {
+      expect(board.placeShip(3, 3, new Ship('Destroyer', 3), 'vertical')).toEqual([[3, 3], [3, 4], [3, 5]]);
+    });
+
+    test('prevented from placing a ship out of bounds', () => {
+      expect(board.placeShip(8, 8, new Ship('Destroyer', 3))).toEqual([]);
+    });
+  })
 });

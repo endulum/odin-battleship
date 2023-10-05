0f62515269b20168f95021fe53b4f1bcb174a5b0
endulum <71853916+endulum@users.noreply.github.com>
2023-10-05 10:28:10 -0700

    can place hits by clicking

--- src/index.js
+++ src/index.js
@@ -2,13 +2,23 @@ import './styles/base.css';
 import './styles/board.css';
 import '@fortawesome/fontawesome-free/js/all.js';
 
-import Player from './modules/player/player';
+import Board from './modules/board/board'
 
-const player = new Player();
-player.yourBoard.placeAllShipsRandomly();
-for (let i = 0; i < 15; i++) player.yourBoard.receiveRandomHit();
+// const player = new Player();
+// player.yourBoard.placeAllShipsRandomly();
 
-document.body.appendChild(renderBoard(player.yourBoard));
+
+const myBoard = new Board();
+myBoard.placeAllShipsRandomly();
+// for (let i = 0; i < 15; i++) myBoard.receiveRandomHit();
+
+// document.body.appendChild(renderBoard(myBoard));
+
+const myBoardDiv = document.createElement('div');
+myBoardDiv.setAttribute('id', 'my-board');
+document.body.appendChild(myBoardDiv);
+
+renderBoard(myBoard);
 
 function renderBoard(boardData) {
   const board = document.createElement('div');
@@ -35,14 +45,16 @@ function renderBoard(boardData) {
         cellHTML += `<div class="y-indicator">${y}</div>`
       }
 
+      // if (
+      //   boardData.isOccupied(x, y) &&
+      //   !boardData.isHit(x, y)
+      // ) {
+      //   cellHTML += `<div class="fill-in ship">
+      //     <i class="fa-solid fa-ship"></i>
+      //   </div>`
+      // } 
+      
       if (
-        boardData.isOccupied(x, y) &&
-        !boardData.isHit(x, y)
-      ) {
-        cellHTML += `<div class="fill-in ship">
-          <i class="fa-solid fa-ship"></i>
-        </div>`
-      } else if (
         !boardData.isOccupied(x, y) &&
         boardData.isHit(x, y)
       ) {
@@ -58,6 +70,14 @@ function renderBoard(boardData) {
         </div>`
       }
 
+      if (boardData.isValidToHit(x, y)) {
+        cell.classList.add('can-hit');
+        cell.addEventListener('click', () => {
+          boardData.receiveHit(x, y);
+          renderBoard(boardData);
+        })
+      }
+
       cell.innerHTML = cellHTML;
       row.appendChild(cell);
     }
@@ -65,7 +85,9 @@ function renderBoard(boardData) {
     board.appendChild(row);
   }
 
-  return board;
+  //return board;
+  myBoardDiv.innerHTML = ``;
+  myBoardDiv.appendChild(board);
 }
 
 const board = document.createElement('div');
